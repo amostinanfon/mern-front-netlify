bf50d91d3323e5da05960fba3983e743a5c29940
amostinanfon <amostinanfon17@gmail.com>
2022-06-09 22:35:40 +0100

    adding api link

--- src/components/Products.js
+++ src/components/Products.js
@@ -21,7 +21,7 @@ const [filteredProducts,setFilteredProducts] = useState([]);
 useEffect(() => {
   const getProducts = async() => {
     try {
-        const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products")
+        const res = await axios.get(cat ? `https://mernecommerces02.herokuapp.com/api/products?category=${cat}` : "https://mernecommerces02.herokuapp.com/api/products")
         setProducts(res.data);
     } catch(err) {
         console.log(err);
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://mernecommerces02.herokuapp.com/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWQ3MjhjM2U2NWQyODE4YmQ4MTcxYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MTcwNzU3MCwiZXhwIjoyNTE1NzA3NTcwfQ.25-9nUMcRUAaFFJg8ZXgT7cm91J-czrwdVdL6A4y93M"
 
 
